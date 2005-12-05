fededa088254e9eb05457057ffe667c7b6ab8ca2
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-12-05 20:24:33 +0000

    Initial checkin.

---
+++ include/hpi_utils.h
@@ -0,0 +1,23 @@
+/*      -*- linux-c -*-
+ *
+ * (C) Copyright IBM Corp. 2005
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  This
+ * file and program are licensed under a BSD style license.  See
+ * the Copying file included with the OpenHPI distribution for
+ * full licensing terms.
+ *
+ * Author:
+ *      David Judkovics <djudkovi@us.ibm.com>
+ *
+ */
+#ifndef _HPI_UTILS_
+#define _HPI_UTILS_
+
+#include <SaHpi.h>
+
+int management_instrument_id(SaHpiRdrT  *rdr);
+
+#endif //_HPI_UTILS_
