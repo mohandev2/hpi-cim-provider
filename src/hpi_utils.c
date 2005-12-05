dda7ab512703580b230d374c16b924eb5861bfb2
djudkovi <djudkovi@a44bbd40-eb13-0410-a9b2-f80f2f72fa26>
2005-12-05 21:05:10 +0000

    *** empty log message ***

--- src/hpi_utils.c
+++ src/hpi_utils.c
@@ -17,24 +17,24 @@
 int management_instrument_id(SaHpiRdrT  *rdr)
 {
 
-        switch (rdr.RdrType) {
+        switch (rdr->RdrType) {
                 case SAHPI_ANNUNCIATOR_RDR:
-                        return(RdrTypeUnion.AnnunciatorRec.AnnunciatorNum);
+                        return(rdr->RdrTypeUnion.AnnunciatorRec.AnnunciatorNum);
                         break;
                 case SAHPI_CTRL_RDR:     
-                        return(rdr.RdrTypeUnion.CtrlRec.Num);
+                        return(rdr->RdrTypeUnion.CtrlRec.Num);
                         break;
                 case SAHPI_INVENTORY_RDR:
-                        return(rdr.RdrTypeUnion.InventoryRec.IdrId);
+                        return(rdr->RdrTypeUnion.InventoryRec.IdrId);
                         break;
                 case SAHPI_NO_RECORD:
-                        return(rdr.RecordId);
+                        return(rdr->RecordId);
                         break;
                 case SAHPI_SENSOR_RDR:
-                        return(rdr.RdrTypeUnion.SensorRec.Num);
+                        return(rdr->RdrTypeUnion.SensorRec.Num);
                         break;
                 case SAHPI_WATCHDOG_RDR:
-                        returnrdr.RdrTypeUnion.WatchdogRec.WatchdogNum);
+                        return(rdr->RdrTypeUnion.WatchdogRec.WatchdogNum);
                         break;
                 default:
                         return(-1);
